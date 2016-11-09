0811f25e788a8031f113300b7281a62da7e912dd
klochenok <romik94@bigmir.net>
2016-11-09 11:24:07 +0200

    new commit

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def post_list(request):
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
 
-@login_required
+
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
