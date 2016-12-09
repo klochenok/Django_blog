351576da9ddc567fd661119fc5713f2a8967b0f2
klochenok <romik94@bigmir.net>
2016-12-09 20:31:11 +0200

    Fix pre-commit errors

--- blog/models.py
+++ blog/models.py
@@ -15,10 +15,8 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
-
 
     def __str__(self):
         return self.title
@@ -34,7 +32,6 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment = True
         self.save()
-
 
     def __str__(self):
         return self.text
