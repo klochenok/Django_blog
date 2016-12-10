be7a429cd839d54928084a8278eaf1540fc48bee
Klochenok Roman <r.klochenok@gmail.com>
2016-12-10 18:36:41 +0200

    Fix errors after TravisCI build failed (#2)
     Fix pre-commit errors and exclude migrations files in config hooks file after Travis build failed.

--- blog/migrations/0002_comment.py
+++ blog/migrations/0002_comment.py
@@ -14,15 +14,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Comment',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('author', models.CharField(max_length=200)),
-                ('text', models.TextField()),
-                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
-                ('approved_comment', models.BooleanField(default=False)),
-                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='blog.Post')),
+        migrations.CreateModel(name='Comment',
+        fields=[
+            ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+            ('author', models.CharField(max_length=200)),
+            ('text', models.TextField()),
+            ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+            ('approved_comment', models.BooleanField(default=False)),
+            ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+            related_name='comments', to='blog.Post')),
             ],
         ),
     ]
--- blog/views.py
+++ blog/views.py
@@ -7,16 +7,14 @@ from .forms import PostForm, CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date'
-        )
+        published_date__lte=timezone.now()).order_by('published_date')
 
-    return render(request, 'blog/post_list.html', {'posts':posts})
-
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
 @login_required
@@ -52,7 +50,7 @@ def post_edit(request, pk):
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
 @login_required
